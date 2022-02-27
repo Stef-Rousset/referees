d3a36d1b42070831d5826a900d3190a856392c3d
stephanie rousset <strabon@club-internet.fr>
2022-02-27 17:04:41 +0100

    add role column to user model, add pundit and configure home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
   validates :encrypted_password, presence: true
 
+  enum role: { normal: 0, contributor: 1, admin: 2}
 end
