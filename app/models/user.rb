8c925e748946a5bd9c2fa4d8c85334f76ba78ec5
stephanie rousset <strabon@club-internet.fr>
2022-02-27 11:29:25 +0100

    add first name and last name columns to user, add validations to user model and update devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true, length: { minimum: 2}
+  validates :last_name, presence: true, length: { minimum: 1}
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :encrypted_password, presence: true
+
 end
