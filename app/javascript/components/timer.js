a5568440caa5891622aa5f1e0a330b701655bbd9
stephanie rousset <strabon@club-internet.fr>
2022-06-21 19:15:54 +0200

    change in timer

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -1,30 +1,27 @@
 const timer = () => {
 
-    const level = document.querySelector('.qcm-title').dataset.level
+    const level = document.querySelector('.qcm-title')
     const minSpan = document.querySelector('.minutes')
     const secSpan = document.querySelector('.seconds')
 
     if(level) {
-        level === 'départemental' ? minSpan.innerHTML = 19 : minSpan.innerHTML = 29
-        secSpan.innerHTML = 59;
-        function updateTimer(){
-            let seconds = 59;
-            let minutes;
-            level === 'départemental' ? minutes = 19 : minutes = 29
-            var timer = setInterval(function(){
-                secSpan.innerHTML = seconds;
-                seconds--;
-                if (seconds === -1) {
-                    seconds = 59;
-                    minutes--;
-                    minSpan.innerHTML = minutes;
-                }
-                if (minutes === 0) {
-                    clearInterval(timer);
-                }
-            }, 1000);
-        }
-        updateTimer();
+        let time;
+        level.dataset.level === 'départemental' ? time = 1200 : time = 1800 // nb de min en secondes
+
+        setInterval(function() {
+            let minutes = parseInt(time / 60, 10);
+            let seconds = parseInt(time % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+            secSpan.innerHTML = seconds
+            minSpan.innerHTML = minutes
+
+            if (--time < 0) {
+                time = 0;
+                // ajouter la logique de soumission du qcm, avec modal informant du résultat
+            }
+        }, 1000);
     }
 }
 
