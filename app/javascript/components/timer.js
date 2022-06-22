67beeb4ed0951a4f9a9e1ac695dc6274a163cd95
stephanie rousset <strabon@club-internet.fr>
2022-06-22 20:06:01 +0200

    change position of timer

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -10,28 +10,28 @@ const timer = () => {
         let time;
         level.dataset.level === 'départemental' ? time = 1200 : time = 1800 // nb de min en secondes
 
-        const interval = setInterval(handleTimer, 1000)
+        const interval = setInterval(handleTimer, 1000); // intervalle d'1 sec en milliseconds
 
-          function handleTimer(){
-            let minutes = parseInt(time / 60, 10);
-            let seconds = parseInt(time % 60, 10);
+        function handleTimer(){
+          let minutes = parseInt(time / 60, 10);
+          let seconds = parseInt(time % 60, 10);
 
-            minutes = minutes < 10 ? "0" + minutes : minutes; // on affiche 09 min et pas 9 min
-            seconds = seconds < 10 ? "0" + seconds : seconds; // on affiche 07 sec et pas 7 sec
-            secSpan.innerHTML = seconds
-            minSpan.innerHTML = minutes
+          minutes = minutes < 10 ? "0" + minutes : minutes; // on affiche 09 min et pas 9 min
+          seconds = seconds < 10 ? "0" + seconds : seconds; // on affiche 07 sec et pas 7 sec
+          secSpan.innerHTML = seconds
+          minSpan.innerHTML = minutes
 
-            // on décrémente time de 1 et quand on arrive sous 60 sec, la div devient rouge
-            // quand on arrive à zero, on arrête l'intervalle et on soumet le formulaire
-            if (time == 0){
-                clearInterval(interval)
-                submitButton.click()  //submit le form
-            } else if (--time < 60 ) {
-                timerDiv.style.backgroundColor = "red";
-            }
+          // on décrémente time de 1 et quand on arrive sous 60 sec, la div devient rouge
+          // quand on arrive à zero, on arrête l'intervalle et on soumet le formulaire
+          if (time == 0){
+              clearInterval(interval)
+              submitButton.click()  //submit le form
+          } else if (time-- < 60 ) {
+              timerDiv.style.backgroundColor = "red";
           }
+        }
         // au clic sur le btn valider, on stoppe le timer
-        submitButton.addEventListener('click', clearInterval(interval))
+        submitButton.addEventListener('click', clearInterval(interval));
     }
 }
 
