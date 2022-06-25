dc9fd42ac0e6e127a4b439c7d21fe8519461346f
stephanie rousset <strabon@club-internet.fr>
2022-06-25 09:54:26 +0200

    modification on forms in home page

--- app/javascript/components/homePageButtons.js
+++ app/javascript/components/homePageButtons.js
@@ -5,37 +5,7 @@ const homePageButtons = () => {
   const filterDivQcm = document.querySelector('.filters-questions-qcm');
   const qcmButton = document.querySelector('.complete-qcm');
   const backButton = document.querySelector('fa-backward');
-  const inputsLevel = document.querySelectorAll('input[name=level]')
-  const inputsCategory = document.querySelectorAll('input[name=category]')
-  const submitQuestionsChoice = document.querySelector('.submit-questions-choice');
-  const submitQcmChoice = document.querySelector('.submit-qcm-choice');
-  let checkedLevel = false
-  let checkedCategory = false
-  // empecher le click sur le btn Commencer si les 2 inputs de choix ne sont pas sélectionnés
-  if (inputsLevel && inputsCategory ) {
-      inputsLevel.forEach(function(input){
-          input.addEventListener('click', function(){
-              if (input.checked){
-                  checkedLevel = true
-                  if (checkedLevel === true && checkedCategory === true){
-                        submitQuestionsChoice.classList.toggle('not-allowed')
-                        submitQcmChoice.classList.toggle('not-allowed')
-                  }
-              }
-          })
-      })
-      inputsCategory.forEach(function(input){
-          input.addEventListener('click', function(){
-              if (input.checked){
-                  checkedCategory = true
-                  if (checkedLevel === true && checkedCategory === true){
-                        submitQuestionsChoice.classList.toggle('not-allowed')
-                        submitQcmChoice.classList.toggle('not-allowed')
-                  }
-              }
-          })
-      })
-  }
+
   // au clic sur le btn Selectionner des questions, montrer le form de choix du
   // level&&category et cacher le btn qcm
   if (selectButton) {
