8bade6adb41783b3badc025ad252bf490e015c93
stephanie rousset <strabon@club-internet.fr>
2022-06-29 19:36:13 +0200

    add welcome text on home page

--- app/javascript/components/homePageButtons.js
+++ app/javascript/components/homePageButtons.js
@@ -5,31 +5,36 @@ const homePageButtons = () => {
   const filterDivQcm = document.querySelector('.filters-questions-qcm');
   const qcmButton = document.querySelector('.complete-qcm');
   const backButton = document.querySelector('fa-backward');
+  const welcomeDiv = document.querySelector('.home-text-welcome');
 
   // au clic sur le btn Selectionner des questions, montrer le form de choix du
-  // level&&category et cacher le btn qcm
+  // level&&category et cacher le texte d'accueil et le btn qcm
   if (selectButton) {
       selectButton.addEventListener('click', function(){
         filterDiv.classList.toggle('hide-element');
         qcmButton.classList.toggle('hide-element');
         selectButton.classList.toggle('hide-element');
+        welcomeDiv.classList.toggle('hide-element');
       })
   }
   // au clic sur le btn Qcm complet, montrer le form de choix du level&&category
-  // et cacher le btn Selectionner des questions
+  // et cacher le le texte d'accueil et le btn Selectionner des questions
   if (qcmButton) {
       qcmButton.addEventListener('click', function(){
         filterDivQcm.classList.toggle('hide-element');
         qcmButton.classList.toggle('hide-element');
         selectButton.classList.toggle('hide-element');
+        welcomeDiv.classList.toggle('hide-element');
       })
   }
-  // au clic sur le btn back, cacher le form et montrer à nouveau les 2 btns
+  // au clic sur le btn back, cacher le form et montrer à nouveau les 2 btns et le
+  // texte d'accueil
   if (backButton) {
       backButton.addEventListener('click', function(){
         filterDiv.classList.toggle('hide-element');
         qcmButton.classList.toggle('hide-element');
         selectButton.classList.toggle('hide-element');
+        welcomeDiv.classList.toggle('hide-element');
       })
   }
 }
